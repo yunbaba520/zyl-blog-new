ed021c25307e519bcb2816ada54b4718144c797a
是云呀 <1650620783@qq.com>
2024-03-12 09:35:46 +0800

    feat: 粒子特效

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
+  <vue-particles id="tsparticles" @particles-loaded="particlesLoaded" :options="particlesOptions" />
   <div class="w-full h-screen flex flex-col justify-between">
     <Header></Header>
     <div class="flex-1 bg-slate-100 dark:bg-gray-900">
       <RouterView></RouterView>
     </div>
+
     <Footer></Footer>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import Header from '@/components/header.vue'
 import Footer from '@/components/footer.vue'
 // change current title
@@ -19,4 +22,85 @@ title.value = 'zyl blog'
 import { useFavicon } from '@vueuse/core'
 const icon = useFavicon()
 icon.value = './src/assets/svg/logo.svg'
+// 粒子特效
+// import { useDark } from '@vueuse/core'
+
+// const isDark = useDark()
+// const color = computed(() => (isDark.value ? '#ff0000' : '#0000ff'))
+const color = ref('#999999')
+const particlesLoaded = async (container) => {
+  console.log('Particles container loaded', container)
+}
+
+const particlesOptions = ref({
+  background: {
+    color: {
+      value: ''
+    }
+  },
+  fpsLimit: 120,
+  interactivity: {
+    events: {
+      onClick: {
+        enable: true,
+        mode: 'push'
+      },
+      onHover: {
+        enable: true,
+        mode: 'repulse'
+      }
+    },
+    modes: {
+      bubble: {
+        distance: 400,
+        duration: 2,
+        opacity: 0.8,
+        size: 40
+      },
+      push: {
+        quantity: 4
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4
+      }
+    }
+  },
+  particles: {
+    color: {
+      value: color.value
+    },
+    links: {
+      color: color.value,
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1
+    },
+    move: {
+      direction: 'none',
+      enable: true,
+      outModes: 'bounce',
+      random: false,
+      speed: 6,
+      straight: false
+    },
+    number: {
+      density: {
+        enable: true
+      },
+      value: 80
+    },
+    opacity: {
+      value: 0.5
+    },
+    shape: {
+      type: 'circle'
+    },
+    size: {
+      value: { min: 1, max: 5 }
+    }
+  },
+  detectRetina: true
+})
 </script>
--- src/main.js
+++ src/main.js
@@ -7,6 +7,9 @@ import i18n from './lang/index'
 import router from './router'
 // icons
 import 'virtual:svg-icons-register'
+// 粒子特效
+import Particles from '@tsparticles/vue3'
+import { loadSlim } from '@tsparticles/slim'
 // md
 import VMdPreview from '@kangc/v-md-editor/lib/preview'
 import '@kangc/v-md-editor/lib/style/preview.css'
@@ -23,5 +26,11 @@ app.use(createPinia())
 // router
 app.use(router)
 app.use(i18n)
+app.use(Particles, {
+  init: async (engine) => {
+    // await loadFull(engine); // you can load the full tsParticles library from "tsparticles" if you need it
+    await loadSlim(engine) // or you can load the slim version from "@tsparticles/slim" if don't need Shapes or Animations
+  }
+})
 
 app.mount('#app')
